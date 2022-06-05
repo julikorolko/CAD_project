79590c87e80b8b70db132a36856d695cbb0d449b
julikorolko <korolko.yuliya@gmail.com>
2022-06-05 18:27:08 +0200

    second commit

--- js/scripts.js
+++ js/scripts.js
@@ -56,4 +56,36 @@
    });
  });
 
+ var selectedClass = "";
+        $(".filter").click(function() {
+            selectedClass = $(this).attr("data-rel");
+            $("#gallery").fadeTo(100, 0.1);
+            $("#gallery div").not("." + selectedClass).fadeOut().removeClass('animation');
+            setTimeout(function() {
+                $("." + selectedClass).fadeIn().addClass('animation');
+                $("#gallery").fadeTo(300, 1);
+            }, 300);
+        });
+
+        $(".tech-icons div")
+            .mouseover(function() {
+                //get p selector with description
+                let p = this.children[1];
+                $(p).css("opacity", 1);
+                $(p).addClass("icons-mouseover");
+                //get i selector 
+                let i = this.children[0];
+                $(i).css("color", "rgba(248, 0, 62, 0.4)");
+                $(i).css("transition", "1s");
+
+            })
+            .mouseout(function() {
+                let p = this.children[1];
+                $(p).css("opacity", 0);
+                $(p).removeClass("icons-mouseover");
+                let i = this.children[0];
+                $(i).css("color", "#212529");
+            });
+
 })(jQuery);
+
